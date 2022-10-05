3f856ffb4e9ddf8f1cd414a936f2778ac383f334
brightGarden02 <rreone2017@gmail.com>
2022-10-05 14:33:31 +0900

    refactor: Spring Configuration을 이용한 캐싱

--- src/main/java/com/example/app__2022_10_04/jwt/JwtProvider.java
+++ src/main/java/com/example/app__2022_10_04/jwt/JwtProvider.java
@@ -1,6 +1,7 @@
 package com.example.app__2022_10_04.jwt;
 
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -8,22 +9,13 @@ import javax.crypto.SecretKey;
 import java.util.Base64;
 
 @Component
+@RequiredArgsConstructor
 public class JwtProvider {
 
-    private SecretKey cachedSecretKey;
-
-    @Value("${custom.jwt.secretKey}")
-    private String secretKeyPlain;
-
-    private SecretKey __getSecretKey() {
-        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
-        return Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
-    }
+    private final SecretKey jwtSecretKey;
 
     public SecretKey getSecretKey() {
-        if(cachedSecretKey == null) cachedSecretKey = __getSecretKey();
-
-        return cachedSecretKey;
+        return jwtSecretKey;
     }
 
 }
